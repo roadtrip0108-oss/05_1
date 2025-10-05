529d4c82e6d3cf939eb2d6be79329099aaa2ba92
roadtrip0108-oss <roadtrip0108@gmail.com>
2025-10-05 21:39:33 +0900

    05_2

--- 05_1.c
+++ 05_1.c
@@ -6,19 +6,14 @@ int main(int argc, char *argv[])
   
   int x;
   
-   printf("Input an integer :");
-   scanf("%d", &x);
-   
-   if (x>0)
-       printf("양 수 입 니 다. \n");
-       
-   else if (x<0)
-        printf("음 수 입 니 다. \n");
-   else 
-        printf("0 입 니 다. \n"); 
-    
-
+  printf("Input an integer :");
+  scanf("%d", &x);
   
+  if(x>0)
+     printf("absolute is %i.\n", x);
+  else
+     printf("absolute is %i.\n", -x);
+     
   system("PAUSE");	
   return 0;
 }
